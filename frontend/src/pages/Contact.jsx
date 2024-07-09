c34f026fba55be4c1f225ab26c4b52bdc450122b
Sark_oo7 <kosinick01@gmail.com>
2024-07-09 16:48:28 +0100

    feat: Update index.css, Contact.jsx, and Signup.jsx
    - Modified index.css to style the form on the contact page.
    - Updated Contact.jsx with HTML structure and elements for customer
    inquiries, complaints, or feedback.
    - Updated Signup.jsx with elements and structure to collect user
    credentials and sign them up.

--- frontend/src/pages/Contact.jsx
+++ frontend/src/pages/Contact.jsx
@@ -2,6 +2,54 @@ import React from 'react'
 
 export const Contact = () => {
   return (
-    <div>Contact</div>
+    <section>
+      <div className=" px-4 mx-auto max-w-screen-md ">
+        <h2 className="heading text-center">
+          Contact Us
+        </h2>
+        <p className="mb08 lg:mb-16 font-light text-center text_para">
+          Got a techinical issue? Want to send feedback about a beta future?
+          Let us know.
+        </p>
+        <form action="#" className="">
+          <div>
+            <label htmlFor="email" className="form__label">
+              Your email
+            </label>
+            <input
+              type="email"
+              id='email'
+              placeholder='example@mail.com'
+              className="form__input mt-1"
+            />
+          </div>
+          <div>
+            <label htmlFor="subject" className="form__label">
+              Subject
+            </label>
+            <input
+              type="text"
+              id='subject'
+              placeholder='Let us know how we can help you'
+              className="form__input mt-1"
+            />
+          </div>
+
+          <div className='sm:col-span-2'>
+            <label htmlFor="message" className="form__label">
+              Message
+            </label>
+            <textarea
+              rows='6'
+              type="text"
+              id='message'
+              placeholder='Leave a comment...'
+              className="form__input mt-1"
+            />
+          </div>
+          <button type='submit' className='btn rounded sm:w-fit'>Submit</button>
+        </form>
+      </div>
+    </section>
   )
 }
