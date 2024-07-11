6399f8f541afe75c125f9d65aaeee14e1e0a2cd5
Sark_oo7 <kosinick01@gmail.com>
2024-07-11 15:34:55 +0100

    feat: Initalize backend with Express and MongoDB - Created index.js to setup Express app with necessary middlewares, connect to MongoDB, and configure the app to listen on a PORT. - Initalized package.json using npm init. - Installed required dependencies.

---
+++ backend/index.js
@@ -0,0 +1,43 @@
+import express from "express";
+import cookieParser from "cookie-parser";
+import cors from "cors";
+import mongoose from "mongoose";
+import dotenv from 'dotenv';
+
+
+dotenv.config()
+
+const app = express();
+const port = process.env.PORT || 8000;
+
+const corsOptions = {
+    origin: true,
+};
+
+// middleware
+app.use(express.json());
+app.use(cookieParser());
+app.use(cors(corsOptions));
+
+
+// database connection
+mongoose.set('strictQuery', false)
+const connectDB = async () => {
+    try {
+        await mongoose.connect(process.env.MONGO_URL)
+
+        console.log("MongoDB is connected");
+    } catch (err) {
+        console.log(`MongoDB connection failed: ${err}`)
+    }
+}
+
+app.get('/', (req, res) => {
+    res.send("api is working")
+});
+
+
+app.listen(port, () => {
+    connectDB();
+    console.log(`Server is running on port ${port}`);
+})
